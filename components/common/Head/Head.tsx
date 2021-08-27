c5f3bbc6442b891896f75d36594f740d4a5eee41
Nhan Thanh Tran <thanhnhan33.95@gmail.com>
2021-08-27 22:38:07 +0700

    style: change app favicon

--- components/common/Head/Head.tsx
+++ components/common/Head/Head.tsx
@@ -9,7 +9,7 @@ const Head = ({ pageName, description }: IHeadProps) => {
         {process.env.NEXT_PUBLIC_APP_NAME} | {pageName}
       </title>
       <meta name="description" content={description} />
-      <link rel="icon" href="/images/earth.svg" />
+      <link rel="icon" href="/images/ecology.svg" />
     </NextHead>
   );
 };
