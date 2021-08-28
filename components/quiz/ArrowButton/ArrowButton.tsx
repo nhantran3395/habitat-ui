a581e4eea65e31b4654522ac99136dac1c12517d
Nhan Thanh Tran <thanhnhan33.95@gmail.com>
2021-08-28 20:22:42 +0700

    refactor: rewrite arrow button

--- components/quiz/ArrowButton/ArrowButton.tsx
+++ components/quiz/ArrowButton/ArrowButton.tsx
@@ -1,22 +1,15 @@
 import React from "react";
 import Image from "next/image";
 import NextLink from "next/link";
-import { Pane, Heading } from "evergreen-ui";
 import styles from "./ArrowButton.module.scss";
 
 const ArrowButton = () => {
   return (
     <NextLink href="/quiz/results">
-      <Pane
-        display="flex"
-        flexDirection="column"
-        alignItems="center"
-        justifyContent="center"
-        className={styles.arrowButton}
-      >
+      <div className={`${styles.arrowButton} flex-column-container`}>
         <Image src="/images/arrow-right.svg" width={45} height={45} />
-        <Heading>View results</Heading>
-      </Pane>
+        <h3>View results</h3>
+      </div>
     </NextLink>
   );
 };
