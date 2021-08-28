86dc46e0b720966b76f98d86cf72f51acf9c21c9
Nhan Thanh Tran <thanhnhan33.95@gmail.com>
2021-08-29 01:42:32 +0700

    feat: add link to resource page of each profile. change cursor to pointer when hovering on habitat

--- pages/my-habitat.tsx
+++ pages/my-habitat.tsx
@@ -23,40 +23,46 @@ const MyHabitat: NextPage = () => {
               <Button margin={majorScale(3)}>Take the Quiz</Button>
             </NextLink>
           </div>
-          <div className={`flex-column-container m-3 w-20 ${styles.habitat}`}>
-            <Image src="/images/savvy-swapper.svg" width={300} height={500} />
-            <h2 className="text-center">Savvy Swapper</h2>
-            <p className="text-center">
-              You thrive on giving preloved clothing a new lease of life and
-              it’s no doubt that you’re called a swapaholic.
-            </p>
-          </div>
-          <div className={`flex-column-container m-3 w-20 ${styles.habitat}`}>
-            <Image
-              src="/images/conscious-consumer.svg"
-              width={400}
-              height={700}
-            />
-            <h2 className="text-center">Conscious Consumer</h2>
-            <p className="text-center">
-              Timelessness, versatility and simplicity are some of your core
-              beliefs. You’re constantly pushing yourself to make wiser choices
-              to go green, whenever it doesn’t slip your mind.
-            </p>
-          </div>
-          <div className={`flex-column-container m-3 w-20 ${styles.habitat}`}>
-            <Image
-              src="/images/curious-explorer.svg"
-              width={300}
-              height={500}
-            />
-            <h2 className="text-center">Curious Explorer</h2>
-            <p className="text-center">
-              Who says being trendy and ethical can’t coexist? Quirky, novel and
-              ever-changing is probably how you would describe your approach to
-              a greener lifestyle.
-            </p>
-          </div>
+          <NextLink href="/resources/savvy-swapper">
+            <div className={`flex-column-container m-3 w-20 ${styles.habitat}`}>
+              <Image src="/images/savvy-swapper.svg" width={300} height={500} />
+              <h2 className="text-center">Savvy Swapper</h2>
+              <p className="text-center">
+                You thrive on giving preloved clothing a new lease of life and
+                it’s no doubt that you’re called a swapaholic.
+              </p>
+            </div>
+          </NextLink>
+          <NextLink href="/resources/conscious-consumer">
+            <div className={`flex-column-container m-3 w-20 ${styles.habitat}`}>
+              <Image
+                src="/images/conscious-consumer.svg"
+                width={400}
+                height={700}
+              />
+              <h2 className="text-center">Conscious Consumer</h2>
+              <p className="text-center">
+                Timelessness, versatility and simplicity are some of your core
+                beliefs. You’re constantly pushing yourself to make wiser
+                choices to go green, whenever it doesn’t slip your mind.
+              </p>
+            </div>
+          </NextLink>
+          <NextLink href="/resources/curious-explorer">
+            <div className={`flex-column-container m-3 w-20 ${styles.habitat}`}>
+              <Image
+                src="/images/curious-explorer.svg"
+                width={300}
+                height={500}
+              />
+              <h2 className="text-center">Curious Explorer</h2>
+              <p className="text-center">
+                Who says being trendy and ethical can’t coexist? Quirky, novel
+                and ever-changing is probably how you would describe your
+                approach to a greener lifestyle.
+              </p>
+            </div>
+          </NextLink>
         </div>
       </Layout>
     </>
