217a32550f35e05bc35fd858cb566254037c967c
Nhan Thanh Tran <thanhnhan33.95@gmail.com>
2021-08-28 19:38:45 +0700

    style: adjust styling

--- components/common/Layout/Layout.tsx
+++ components/common/Layout/Layout.tsx
@@ -7,34 +7,18 @@ import styles from "./Layout.module.scss";
 
 const Layout = ({ children }: ILayoutProps) => {
   return (
-    <Pane
-      width="100vw"
-      height="100vh"
-      display="flex"
-      alignItems="center"
-      justifyContent="center"
-      justifySelf="center"
-      flexDirection="column"
-    >
-      <Pane className={styles.bottomLeftCorner}>
-        <Image src="/images/bottom-left-corner.svg" width={420} height={330} />
-      </Pane>
+    <div className="flex-column-container w-100">
+      <div className={styles.bottomLeftCorner}>
+        <Image src="/images/bottom-left-corner.svg" width={419} height={330} />
+      </div>
 
-      <Pane className={styles.topRightCorner}>
-        <Image src="/images/top-right-corner.svg" width={420} height={330} />
-      </Pane>
+      <div className={styles.topRightCorner}>
+        <Image src="/images/top-right-corner.svg" width={419} height={330} />
+      </div>
 
       <Header />
-      <Pane
-        width="80%"
-        height="80%"
-        display="flex"
-        alignItems="center"
-        justifyContent="center"
-      >
-        {children}
-      </Pane>
-    </Pane>
+      <div className="flex-container-column w-80">{children}</div>
+    </div>
   );
 };
 
--- components/common/SocialNetworkBar/SocialNetworkBar.tsx
+++ components/common/SocialNetworkBar/SocialNetworkBar.tsx
@@ -1,24 +1,15 @@
 import React from "react";
 import Image from "next/image";
-import { Pane, majorScale } from "evergreen-ui";
 import styles from "./SocialNetworkBar.module.scss";
 
 const SocialNetworkBar = () => {
   return (
-    <Pane
-      display="flex"
-      flexDirection="row"
-      alignItems="center"
-      justifyContent="center"
-      className={styles.bottomRightCorner}
-    >
+    <div className={styles.bottomRightCorner}>
       <Image src="/images/facebook.svg" width={54} height={54} />
       <Image src="/images/twitter.svg" width={47} height={47} />
       <Image src="/images/instagram.svg" width={46} height={46} />
-      <Pane marginLeft={majorScale(1)}>
-        <Image src="/images/whatsapp.svg" width={38} height={38} />
-      </Pane>
-    </Pane>
+      <Image src="/images/whatsapp.svg" width={38} height={38} />
+    </div>
   );
 };
 
