1911fb3b17825185aaf5862f33b7bee637e12e6c
Nhan Thanh Tran <thanhnhan33.95@gmail.com>
2021-08-29 01:59:41 +0700

    feat: add our mission page

--- pages/our-mission.tsx
+++ pages/our-mission.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from "next";
-import { Pane } from "evergreen-ui";
+import Image from "next/image";
+import NextLink from "next/link";
 import Head from "../components/common/Head";
 import Layout from "../components/common/Layout";
 
@@ -8,7 +9,20 @@ const OurMission: NextPage = () => {
     <>
       <Head pageName="our mission" description="" />
       <Layout>
-        <Pane>Our Mission</Pane>
+        <div className="flex-row-container">
+          <div className="w-40 m-3">
+            <h1>our mission</h1>
+
+            <p className="w-80">
+              Paving the way for environmentally responsible lifestyles -
+              empower individuals to drive change towards sustainability
+              starting with simple lifestyle habits.
+            </p>
+          </div>
+          <div className="w-40">
+            <Image src="/images/workshops.svg" width={600} height={600} />
+          </div>
+        </div>
       </Layout>
     </>
   );
