b2dc86787253915894ca0e53552e51d11bd6d22b
Nhan Thanh Tran <thanhnhan33.95@gmail.com>
2021-08-28 18:10:08 +0700

    style: replace link component used for app name with anchor tag

--- components/common/Header/Header.tsx
+++ components/common/Header/Header.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import NextLink from "next/link";
-import { Pane, Heading, Link, majorScale } from "evergreen-ui";
 import NavigationBar from "../NavigationBar";
 
 const appName = process.env.NEXT_PUBLIC_APP_NAME;
@@ -9,15 +8,9 @@ const Header = () => {
   return (
     <div className="w-80 h-20 flex-column-container">
       <NextLink href="/">
-        <Link
-          display="flex"
-          alignItems="center"
-          justifyContent="space-between"
-          margin={majorScale(2)}
-          cursor="pointer"
-        >
-          <Heading size={900}>{appName}</Heading>
-        </Link>
+        <a>
+          <h1>{appName}</h1>
+        </a>
       </NextLink>
       <NavigationBar />
     </div>
