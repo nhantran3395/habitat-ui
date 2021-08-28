5441d0768eb8e8a47ccf9833e4051cd3cc4a18f4
Nhan Thanh Tran <thanhnhan33.95@gmail.com>
2021-08-28 08:03:34 +0700

    style: adjust style of Option component

--- components/quiz/Option/Option.tsx
+++ components/quiz/Option/Option.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import { Button as ButtonUI, ButtonProps } from "evergreen-ui";
+import { Paragraph as ParagraphUI, ParagraphProps } from "evergreen-ui";
 import styles from "./Option.module.scss";
 
-const Option = (props: ButtonProps) => {
-  return <ButtonUI {...props} className={styles.option} />;
+const Option = (props: ParagraphProps) => {
+  return <ParagraphUI {...props} className={styles.option} />;
 };
 
 export default Option;
