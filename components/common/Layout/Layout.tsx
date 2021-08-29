9776f178687a0c195a5d8585194a172a7bb45b00
Nhan Thanh Tran <thanhnhan33.95@gmail.com>
2021-08-29 09:13:54 +0700

    style: make bottom left illustration sticky

--- components/common/Layout/Layout.tsx
+++ components/common/Layout/Layout.tsx
@@ -8,16 +8,16 @@ import styles from "./Layout.module.scss";
 const Layout = ({ children }: ILayoutProps) => {
   return (
     <div className="flex-column-container w-100">
-      <div className={styles.bottomLeftCorner}>
-        <Image src="/images/bottom-left-corner.svg" width={419} height={330} />
-      </div>
-
       <div className={styles.topRightCorner}>
         <Image src="/images/top-right-corner.svg" width={419} height={330} />
       </div>
 
       <Header />
       <div className="flex-container-column w-80">{children}</div>
+
+      <div className={styles.bottomLeftCorner}>
+        <Image src="/images/bottom-left-corner.svg" width={419} height={330} />
+      </div>
     </div>
   );
 };
