8385ea98b274a9f154c8613c06b4e4afb30bcda4
Nhan Thanh Tran <thanhnhan33.95@gmail.com>
2021-09-03 09:36:38 +0700

    style: make layout and body container 100vh at minimum

--- components/common/Layout/Layout.tsx
+++ components/common/Layout/Layout.tsx
@@ -7,14 +7,14 @@ import styles from "./Layout.module.scss";
 
 const Layout = ({ children }: ILayoutProps) => {
   return (
-    <div className="flex-column-container w-100">
+    <div className="flex-column-container w-100 min-h-100vh">
       <div className={styles.topRightCorner}>
         <Image src="/images/top-right-corner.svg" width={419} height={330} />
       </div>
 
       <Header />
 
-      <div className="flex-container-column w-80">{children}</div>
+      <div className="flex-container-column w-80 min-h-100vh">{children}</div>
 
       <SocialNetworkBar />
 
