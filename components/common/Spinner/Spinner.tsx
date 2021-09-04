7a1f9c79fc875befabf09f7fc59f441806fecd3d
Nhan Thanh Tran <thanhnhan33.95@gmail.com>
2021-09-04 21:50:05 +0700

    style: reduce spinner size

--- components/common/Spinner/Spinner.tsx
+++ components/common/Spinner/Spinner.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Loader from "react-loader-spinner";
 
 const Spinner = () => {
-  return <Loader type="Oval" color="#1a6652" />;
+  return <Loader type="Oval" color="#1a6652" width={50} height={50} />;
 };
 
 export default Spinner;
