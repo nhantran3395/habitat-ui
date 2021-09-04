11bee3ab0cb39abce356f3d5257623a52fb5e786
Nhan Thanh Tran <thanhnhan33.95@gmail.com>
2021-09-04 21:45:12 +0700

    refactor: remove getUserId action from userProfileSlice

--- features/userProfile/interfaces.ts
+++ features/userProfile/interfaces.ts
@@ -1,5 +1,4 @@
 export interface IUserProfileState {
-  userId: string;
   profileName: string;
   isPending: boolean;
   isError: boolean;
--- features/userProfile/userProfileSlice.ts
+++ features/userProfile/userProfileSlice.ts
@@ -4,30 +4,12 @@ import {
 } from '@reduxjs/toolkit';
 import type { RootState } from '../../app/store';
 import { IUserProfileState } from './interfaces';
-import { ISelection } from '../../models/quiz'
 
 const initialState: IUserProfileState = {
-  userId: '',
   profileName: '',
   isPending: false,
   isError: false,
 };
-
-export const getUserId = createAsyncThunk('userProfile/getUserId', async (answers: ISelection[]) => {
-  console.log(JSON.stringify(answers));
-
-  const response = await fetch(`${process.env.NEXT_PUBLIC_APP_HABITAT_API}/answer/answerQuestions`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(answers),
-  });
-  const data = await response.json();
-
-  console.log('userId response', data);
-  return data;
-});
 
 export const getProfileByUserId = createAsyncThunk('userProfile/getProfileByUserId', async (userId: string) => {
   const response = await fetch(`${process.env.NEXT_PUBLIC_APP_HABITAT_API}/answer/userProfiles?userId=${userId}`,);
@@ -43,17 +25,6 @@ export const userProfile = createSlice({
   },
   extraReducers: builder => {
     builder
-      .addCase(getUserId.pending, state => {
-        state.isPending = true;
-        state.isError = false;
-      })
-      .addCase(getUserId.fulfilled, (state, { payload }) => {
-        state.userId = payload.userId;
-      })
-      .addCase(getUserId.rejected, state => {
-        state.isPending = false;
-        state.isError = true;
-      })
       .addCase(getProfileByUserId.pending, state => {
         state.isPending = true;
         state.isError = false;
